efa72f35ed0e4d57d19288206f1fd2ca6a7f5751
Gerard <gerard@protofy.xyz>
2023-11-16 14:27:55 +0100

    deletes thing

--- packages/app/bundles/test/context/useTestStore.ts
+++
@@ -1,9 +0,0 @@
-import { atom, useAtom } from 'jotai'
-
-const TestAtom = atom("Palomita")
-
-export const useTestStore = () => {
-  const [test, setTest] = useAtom(TestAtom)
-
-  return { test, setTest }
-}
