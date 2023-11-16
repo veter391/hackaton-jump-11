9896b09ddaef54d177b9689df5d44502d85d06d3
Gerard <gerard@protofy.xyz>
2023-11-16 23:48:56 +0100

    adjust  and add type

--- apps/web/src/Components/Form.tsx
+++ apps/web/src/Components/Form.tsx
@@ -1,18 +1,29 @@
 import { Teacher } from '../models/question/teacher.model'
 import '../styles/form.css'
-import { Button, TextField, Typography } from '@mui/material';
+import { Button, TextField, Typography, FormControl, Select, InputLabel, MenuItem } from '@mui/material';
 import { useState } from 'react';
 
 export function Form({ onSumit }: { onSumit: (teacher: Teacher) => void }) {
 
   const onPressGenerate = () => {
     // new object for get data to chat
+    console.log('DEV: ', data)
     onSumit(data)
   }
   const [data, setData] = useState<any>({})
 
   const onInputChange = (e: any, key: string) => {
-    setData({ ...data, [key]: e.target.value })
+    const val = e.target.value
+    let newData = { ...data, [key]: val}
+    if (key == "teacherType") {
+      const studentAges: any = {
+        "infantil": "6",
+        "primaria": "12",
+        "eso": "16"
+      }
+      newData = { ...newData, "averageStudentsAge": studentAges[val] ?? "16"}
+    }
+    setData(newData)
   }
 
   return (
@@ -22,13 +33,25 @@ export function Form({ onSumit }: { onSumit: (teacher: Teacher) => void }) {
           Introduce tus datos
         </Typography>
         <TextField style={{ marginTop: 20, }} onChange={(e) => onInputChange(e, "name")} id="outlined-basic" label="Nombre" variant="outlined" />
-        <TextField style={{ marginTop: 12 }} onChange={(e) => onInputChange(e, "teacherType")} id="outlined-basic" label="Tipo" variant="outlined" />
+        <FormControl style={{ marginTop: 12 }} fullWidth>
+          <InputLabel id="demo-simple-select-label">Age</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={data.teacherType ?? "infantil"}
+            label="Ciclo educativo"
+            onChange={(e) => onInputChange(e, "teacherType")}
+          >
+            <MenuItem value={"infantil"}>Infantil</MenuItem>
+            <MenuItem value={"primaria"}>Primaria</MenuItem>
+            <MenuItem value={"eso"}>ESO</MenuItem>
+          </Select>
+        </FormControl>
         <TextField style={{ marginTop: 12 }} onChange={(e) => onInputChange(e, "locality")} id="outlined-basic" label="Localidad" variant="outlined" />
         <TextField style={{ marginTop: 12 }} onChange={(e) => onInputChange(e, "districtCode")} id="outlined-basic" label="Nº distrito" variant="outlined" />
         <TextField style={{ marginTop: 12 }} onChange={(e) => onInputChange(e, "neighborhoodCode")} id="outlined-basic" label="Barrio" variant="outlined" />
-        <TextField style={{ marginTop: 12 }} onChange={(e) => onInputChange(e, "studentsAmount")} id="outlined-basic" label="Nº estudiantes" variant="outlined" />
-        <TextField style={{ marginTop: 12, marginBottom: 40 }} onChange={(e) => onInputChange(e, "averageStudentsAge")} id="outlined-basic" label="Edad media estudiantes" variant="outlined" />
-        <Button onClick={onPressGenerate} variant="contained" style={{ height: 42}}>Generar asistente</Button>
+        <TextField style={{ marginTop: 12, marginBottom: 40 }} onChange={(e) => onInputChange(e, "studentsAmount")} id="outlined-basic" label="Nº estudiantes" variant="outlined" />
+        <Button onClick={onPressGenerate} variant="contained" style={{ height: 42 }}>Generar asistente</Button>
       </div>
     </div>
   )
