984b1f0c77ca4398225d80e37e0dc462752634cd
Gerard <gerard@protofy.xyz>
2023-11-16 12:35:01 +0100

    deletes next app and ui

--- packages/ui/src/components/DegradatedText.tsx
+++
@@ -1,7 +0,0 @@
-const DegradatedText = ({children}) => {
-    return(
-        <span className="all ease-in degradatedText ms250 rainbow clip-text">{children}</span>
-    )
-}
-
-export default DegradatedText
--- packages/ui/src/components/RainbowText.tsx
+++
@@ -1,7 +0,0 @@
-const RainbowText = ({children}) => {
-    return(
-        <span className="all ease-in ms250 rainbow clip-text">{children}</span>
-    )
-}
-
-export default RainbowText
