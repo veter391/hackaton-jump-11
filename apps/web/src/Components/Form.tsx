7ec52e3e69d83d9ba63634919e1a227b69da4e85
veter391 <nazarcova391@gmail.com>
2023-11-16 21:20:09 +0100

    changed form

--- apps/web/src/Components/Form.tsx
+++ apps/web/src/Components/Form.tsx
@@ -9,17 +9,18 @@ export function Form({ onSumit}: { onSumit : (teacher: Teacher) => void}) {
     handleSubmit,
   } = useForm()
 
-  const onSubmit = (data: any ) => {
+  const onSubmit = (data : any) => {
+    const { name, lastName, teacherType, locality, districtCode, neighborhoodCode, studentsAmount, averageStudentsAge } = data;
     // new object for get data to chat
     const newData = {
-      name: data.teacher,
-      // lastName: data.,
-    //   teacherType: data.,
-    //   locality: data.,
-    //   districtCode: data.,
-    //   neighborhoodCode: data.,
-    //   studentsAmount: data.,
-    //   averageStudentsAge: data.
+      name,
+      lastName,
+      teacherType,
+      locality,
+      districtCode,
+      neighborhoodCode,
+      studentsAmount,
+      averageStudentsAge
     }
     console.log(data, newData)
     onSumit(data)
@@ -29,34 +30,84 @@ export function Form({ onSumit}: { onSumit : (teacher: Teacher) => void}) {
     <section>
       <form className="form" onSubmit={handleSubmit(onSubmit)}>
         <h2> Introduce tus datos </h2>
-        <input className="form__input" type="text" name="classTopic" placeholder="Apellido de profesor" />
+        {/* <input className="form__input" type="text" name="classTopic" placeholder="Apellido de profesor" />
         <input className="form__input" type="text" name="classTopic" placeholder="Tipo de Profesor" />
-        <input className="form__input" type="text" name="classTopic" placeholder="Asignatura que imparte" />
+        <input className="form__input" type="text" name="classTopic" placeholder="Asignatura que imparte" /> */}
         
         <input className="form__input" type="text" placeholder="Nombre del profesor"
-          {...register('teacher', {
+          {...register('name', {
             required: true,
             pattern: /^[a-zA-ZÁ-ú\s]+$/i,
           })}
         />
 
-        <input
+        <input className="form__input" type="text" placeholder="Apellido del profesor"
+          {...register('lastName', {
+            required: true,
+            pattern: /^[a-zA-ZÁ-ú\s]+$/i,
+          })}
+        />
+
+        <input className="form__input" type="text" placeholder="Tipo de profesor"
+          {...register('teacherType', {
+            // required: true,
+            pattern: /^[a-zA-ZÁ-ú\s]+$/i,
+          })}
+        />
+
+        <input className="form__input" type="text" placeholder="Barrio"
+          {...register('locality', {
+            required: true,
+            pattern: /^[a-zA-ZÁ-ú\s]+$/i,
+          })}
+        />
+
+        <input className="form__input" type="text" placeholder="Codigo de area"
+          {...register('districtCode', {
+            // required: true,
+            pattern: /^[a-zA-ZÁ-ú\s]+$/i,
+          })}
+        />
+
+
+        <input className="form__input" type="text" placeholder="neighborhoodCode"
+          {...register('neighborhoodCode', {
+            // required: true,
+            pattern: /^[a-zA-ZÁ-ú\s]+$/i,
+          })}
+        />
+
+        <input className="form__input" type="text" placeholder="Cantidad de alumnos"
+          {...register('studentsAmount', {
+            // required: true,
+            pattern: /^[a-zA-ZÁ-ú\s]+$/i,
+          })}
+        />
+
+        <input className="form__input" type="text" placeholder="edad de alumnos"
+          {...register('averageStudentsAge', {
+            // required: true,
+            pattern: /^[a-zA-ZÁ-ú\s]+$/i,
+          })}
+        />
+
+        {/* <input
           className="form__input" type="text" placeholder="Nombre del colegio"
           {...register('school', {
             required: true,
             pattern: /^[a-zA-ZÁ-ú\s]+$/i,
           })}
-        />
+        /> */}
 
         {errors.school?.type === 'required' && <p style={{color: 'red'}}> Se require nombre del colegio</p>}
         {errors.school?.type === 'pattern' && <p style={{color: 'red'}}> El nombre no es válido</p>}
         {errors.teacher?.type === 'required' && <p style={{color: 'red'}}> Se require nombre del profesor</p>}
         {errors.teacher?.type === 'pattern' && <p style={{color: 'red'}}> El nombre no es válido</p>}
-        <input className="form__input" type="text" name="barrio" placeholder="Barrio" />
+        {/* <input className="form__input" type="text" name="barrio" placeholder="Barrio" />
         <input className="form__input" type="text" name="barrio" placeholder="Codigo del barrio" />
         <input className="form__input" type="text" name="classTopic" placeholder="Asignatura que imparte"/>
         <input className="form__input" type="text" name="numberStudents" placeholder="Edad de alumnos"/>
-        <input className="form__input" type="text" name="numberStudents" placeholder="Numero de alumnos" />
+        <input className="form__input" type="text" name="numberStudents" placeholder="Numero de alumnos" /> */}
 
         <button className="form__button">continuar</button>
       </form>
