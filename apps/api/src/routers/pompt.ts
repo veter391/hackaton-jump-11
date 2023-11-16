72e437f05d05b8e0318cd8bf0a6ed479d7ea13fc
Joel Campos <joelcamposoliva9000@gmail.com>
2023-11-16 13:22:45 +0100

    User service

--- apps/api/src/routers/pompt.ts
+++ apps/api/src/routers/pompt.ts
@@ -1,9 +1,11 @@
+import { QuestionService } from '@/services/prompt/question.service';
 import express from 'express';
 
 const router = express.Router();
+const questionService = new QuestionService();
 
 router.get('/api/v1/ask', (req, res) => {
-    res.json({});
+    res.json(questionService.ask(req.body));
 });
 
 export default router;
---
+++ apps/api/src/services/prompt/question.service.ts
@@ -0,0 +1,7 @@
+import { QuestionDTO } from "@/dtos/prompt/question.dto";
+
+export class QuestionService {
+    async ask(question: QuestionDTO): Promise<{}> {
+        return {};
+    }
+}
